14ce85261192d4e952a5667bab4a5d8378502748
Ashvini379 <44570192+Ashvini379@users.noreply.github.com>
2019-12-22 11:23:51 +0530

    Add project files.

---
+++ SignupWithLogin/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SignupWithLogin.Startup))]
+namespace SignupWithLogin
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
